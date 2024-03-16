1328db8eee7e2ea5b4e774f1d4b8240b269fa29e
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2024-03-16 23:08:12 +0530

    header completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TopBar from "./topbar/TopBar";
+
+function App() {
+  return (
+    //we can not use multiple element inside a componet so use <>
+    <>
+    <TopBar/>
+    </>
+  )
+}
+
+export default App;
