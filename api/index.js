e553cd5434d3190092123a901b27c30def4a62d0
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2024-03-18 23:53:35 +0530

    auth completed

---
+++ api/index.js
@@ -0,0 +1,26 @@
+//express server
+const express=require("express");
+const app=express()
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const authRoute = require("./routes/auth");
+//authentication path
+
+dotenv.config();
+//this application is able to send the json object
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Successfully connected to MongoDB");
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+
+app.use("/api/auth", authRoute);
+
+app.listen("5000", ()=>{
+    console.log("Backend is running");
+})
---
+++ api/routes/auth.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+//REGISTER
+router.post("/register", async (req, res)=>{
+    try{
+
+        const salt=await bcrypt.genSalt(10);
+        const hashedPass=await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashedPass,
+        })
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+router.post("/login", async(req, res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username})
+        //if there is no user
+        !user && res.status(400).json("Wrong credentials!")
+
+        //if there is user, we should validate the password
+        //(sent password, password which we have hashed inside the mongodb)
+        const validate=await bcrypt.compare(req.body.password, user.password);
+        //if not validated
+        !validate && res.status(400).json("Wrong credentials!")
+        
+        //it will take everything but not the password
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
