3d77a1e484e1fb28e827b79ae2259246e5f11177
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2024-03-24 14:30:55 +0530

    rendering completed for categories/user posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,40 +1,42 @@
 import "./post.css";
 import Img from "../../static/photo.jpg"
 import { Link } from "react-router-dom";
-function Post() {
+function Post({post}) {
     return (
         <div className='posts'>
+        {post.photo ? (
+            <img
+                className="postImg"
+                src={post.photo}
+                alt=""
+            />
+        ) : (
             <img
                 className="postImg"
                 src={Img}
-                alt=""
+                alt="Default"
             />
+        )}
+
             <div className="postInfo">
-                <div className="postCats">
-                <span className="postCat">
-                    <Link className="link" to="/posts?cat=Music">
-                    Music
-                    </Link>
-                </span>
-                <span className="postCat">
-                    <Link className="link" to="/posts?cat=Music">
-                    Life
-                    </Link>
-                </span>
+                <div className="postCats">{
+                    post.categories.map(c=>(
+                        <span className="postCat">
+                            {c.name}
+                        </span>
+                    ))
+                }
                 </div>
-                <span className="postTitle">
-                <Link to="/post/abc" className="link">
-                    Lorem ipsum dolor sit amet
+                <Link to={`/post/${post._id}`} className="link">
+                    <span className="postTitle">
+                        {post.title}
+                    </span>
                 </Link>
-                </span>
                 <hr />
-                <span className="postDate">1 hour ago</span>
+                <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
             </div>
             <p className="postDesc">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-                officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-                fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-                atque, exercitationem quibusdam, reiciendis odio laboriosam?
+               {post.desc}
             </p>
         </div>
     );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,13 +2,28 @@ import "./home.css";
 import Header from '../../components/header/Header';
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/siderbar/Sidebar";
+import { useEffect, useState } from "react";
+import {useLocation} from "react-router";
+import axios from "axios";
 
 function Home() {
+    const [posts, setPosts] = useState([]);
+    const {search} = useLocation();
+
+    // console.log(location);
+
+    useEffect(()=>{
+        const fetchPosts = async()=>{
+            const res = await axios.get("http://localhost:3000/posts" + search)
+            setPosts(res.data)
+        }
+        fetchPosts()
+    },[search])
     return (
         <>
             <Header/>
             <div className="home" >
-                <Posts/>
+                <Posts posts={posts}/>
                 <Sidebar/>
             </div>
         </>
