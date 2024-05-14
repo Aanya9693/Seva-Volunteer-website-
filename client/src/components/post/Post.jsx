27a38ae2da9c889154a6c4825e97768f7a5ec0a5
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2024-05-14 15:55:02 +0530

    e.map issue resolved

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -22,14 +22,14 @@ function Post({post}) {
             <div className="postInfo">
                 <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
                 
-                <div className="postCats">{
-                    post.categories.map(c=>(
-                        <span className="postCat">
+                <div className="postCats">
+                    {Array.isArray(post.categories) && post.categories.map(c => (
+                        <span className="postCat" key={c._id}>
                             {c.name}
                         </span>
-                    ))
-                }
+                    ))}
                 </div>
+
                 <Link to={`/post/${post._id}`} className="link">
                     <span className="postTitle">
                         {post.title}
