7e5247965e4194b75f262c2ccd7761b4fd7dc65e
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2024-06-27 17:00:40 +0530

    apis changed

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,14 +1,20 @@
+import PropTypes from "prop-types";
 import Post from "../post/Post";
 import "./posts.css";
 
-function Posts({posts}) {
+function Posts({ posts }) {
+    console.log("Posts component received posts:", posts); // Log the posts received by the component
     return (
         <div className="posts">
-            {posts.map((p, i) =>(
-                <Post post={p} key={i}/>
+            {posts.map((post, index) => (
+                <Post key={index} post={post} />
             ))}
-      </div>
+        </div>
     );
 }
+
+Posts.propTypes = {
+    posts: PropTypes.array.isRequired,
+};
 
 export default Posts;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,28 +3,38 @@ import Header from '../../components/header/Header';
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/siderbar/Sidebar";
 import { useEffect, useState } from "react";
-import {useLocation} from "react-router";
+import { useLocation } from "react-router";
 import axios from "axios";
 
 function Home() {
     const [posts, setPosts] = useState([]);
-    const {search} = useLocation();
+    const { search } = useLocation();
 
-    // console.log(location);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await axios.get("https://seva-kappa.vercel.app/posts" + search);
+                console.log("Fetched posts:", res.data[0]); // Log the response data
+                if (Array.isArray(res.data)) {
+                    setPosts(res.data);
+                } else {
+                    console.error("Fetched data is not an array:", res.data);
+                    setPosts([]); // Ensure posts is set to an empty array if data is not as expected
+                }
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+                setPosts([]); // Set posts to an empty array in case of error
+            }
+        };
+        fetchPosts();
+    }, [search]);
 
-    useEffect(()=>{
-        const fetchPosts = async()=>{
-            const res = await axios.get("http://localhost:3000/posts" + search)
-            setPosts(res.data)
-        }
-        fetchPosts()
-    },[search])
     return (
         <>
-            <Header/>
-            <div className="home" >
-                <Posts posts={posts}/>
-                <Sidebar/>
+            <Header />
+            <div className="home">
+                <Posts posts={posts} />
+                <Sidebar />
             </div>
         </>
     );
